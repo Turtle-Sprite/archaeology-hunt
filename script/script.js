9631091afb33d46a006c9ef8f7a426e654dfa254
Turtle-Sprite <crystal.sheeley13@gmail.comcd>
2022-12-01 23:04:24 -0800

    added some timers to countdown to volcanic eruption

--- script/script.js
+++ script/script.js
@@ -19,6 +19,7 @@ let resetBtn = document.querySelector('#reset')
 const ctx = canvas.getContext('2d')
 console.log(ctx)
 
+//=====Drawing Class====//
 //creating the players/game pieces
 class Drawing {
 
@@ -30,7 +31,6 @@ class Drawing {
         this.height = height
         this.color = color
     }
-
     //when calling this class, we'll input the above variables, then render will draw it using Rect
     renderRect () {
         ctx.fillStyle = this.color
@@ -38,18 +38,21 @@ class Drawing {
     }        
 }
 
+// ===== Draw Character====//
 //creates the "archaeologist character"
 const archChar = new Drawing(50,50, 25, 25, 'orange')
 //calls the character to be drawn
 archChar.renderRect()
 
-
-//creating an empty array for pressedKeys
+//====== Handling movement ======///
+//creating an empty object for pressedKeys so we can call them in the function below
 const pressedKeys = {}
+// console.log(pressedKeys)
 //creating movement for archaeologist function
     //needs e-event for knowing which key was pressed
 function handleMovement () {
-    console.log('handle movement', )
+    // console.log('handle movement', )
+    // console.log(archChar)
     const speed = 5
     //move down
     if (pressedKeys.w) {
@@ -67,7 +70,10 @@ function handleMovement () {
     if (pressedKeys.d) {
         archChar.x += speed
     }
+    archChar.renderRect()
 }
+
+//===== Event Listeners for key press ======//
 //if a key is pressed, pressedKeys *empty object* updates e *with the key pressed *.  e.key
 //e is event, key is the property
 document.addEventListener('keydown', e => pressedKeys[e.key] = true)
@@ -75,4 +81,65 @@ document.addEventListener('keydown', e => pressedKeys[e.key] = true)
 document.addEventListener('keyup', e => pressedKeys[e.key] = false)
 
 //handle movement can access the pressedKeys e.key anytime because it is a global object
-handleMovement()
+
+
+//======== Timers =========///
+//game will loop every 60 miliseconds until we clear the interval (later)
+//with no clearRect, we get a snake movement with a trail of where object has been
+//invoke handleMovement to make sure we can use the function repeatedly
+setInterval( 
+    function gameLoop () {
+        ctx.clearRect(0,0, canvas.width, canvas.height)
+        handleMovement()
+    }, 60)
+
+//Game timer - 2 minutes to complete level, check for win scenario
+setInterval (
+    function timer () {
+        
+    }, 120000) //2 minutes 2 * 60 *1000
+
+
+    //======count up to 2 minutes=====//
+//every second update clock
+// call with start button
+//clear when Game Timer runs out or player hits a death trap
+//could also use this as GameTimer and use else to log out player win/lose scenario
+// let time = 0
+// let volacanoClock = setInterval (
+//     function timer () {
+//         if (time < 10) {
+//             volcanoTimer.innerText = `0:0${time}`
+//         } else if (time < 60) {
+//             volcanoTimer.innerText = `0:${time}`
+//         } else if (time >= 60 && time < 70) {
+//             volcanoTimer.innerText = `1:0${time - 60}`
+//         } else if (time >= 70 && time <= 120){
+//                 volcanoTimer.innerText = `1:${time - 60}`
+//         }
+//         time++
+//     }, 500)
+
+    //=====countdown from 2 minutes
+    let countdown = 120
+    let volacanoClockDown = setInterval (
+        function timer () {
+            console.log('countdown ', countdown)
+            console.log(archChar.x, archChar.y)
+            if (countdown === 120) {
+                volcanoTimer.innerText = `2:00`
+            } else if (countdown < 120 && countdown>= 70) {
+                volcanoTimer.innerText = ` 1:${countdown - 60}`
+            } else if (countdown >= 60 && countdown < 70) {
+                volcanoTimer.innerText = ` 1:0${countdown - 60}`
+            } else if (countdown < 60 && countdown >= 10) {
+                volcanoTimer.innerText = ` 0:${countdown}`
+            } else if (countdown< 10 && countdown>= 0){
+                    volcanoTimer.innerText = `0:0${countdown}`
+            } else {
+                clearInterval(volacanoClockDown)
+            }
+            countdown--
+        }, 500)
+
+
