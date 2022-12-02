4e32e4c5d3e1eaf6afc24e3cbf28a22db19d5f9c
Turtle-Sprite <crystal.sheeley13@gmail.comcd>
2022-12-01 17:38:31 -0800

    Created a handle movement function, currently functional. Need to create a game loop for it to be called and allow character to move

--- script/script.js
+++ script/script.js
@@ -38,6 +38,41 @@ class Drawing {
     }        
 }
 
+//creates the "archaeologist character"
 const archChar = new Drawing(50,50, 25, 25, 'orange')
+//calls the character to be drawn
 archChar.renderRect()
 
+
+//creating an empty array for pressedKeys
+const pressedKeys = {}
+//creating movement for archaeologist function
+    //needs e-event for knowing which key was pressed
+function handleMovement () {
+    console.log('handle movement', )
+    const speed = 5
+    //move down
+    if (pressedKeys.w) {
+        archChar.y -= speed
+    }
+    //move up
+    if (pressedKeys.s) {
+        archChar.y+= speed
+    }
+    //move left
+    if (pressedKeys.a) {
+        archChar.x -= speed
+    }
+    //move right
+    if (pressedKeys.d) {
+        archChar.x += speed
+    }
+}
+//if a key is pressed, pressedKeys *empty object* updates e *with the key pressed *.  e.key
+//e is event, key is the property
+document.addEventListener('keydown', e => pressedKeys[e.key] = true)
+//if key is up = false
+document.addEventListener('keyup', e => pressedKeys[e.key] = false)
+
+//handle movement can access the pressedKeys e.key anytime because it is a global object
+handleMovement()
